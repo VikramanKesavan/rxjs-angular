d4f1a895d09a25956ff3624289bd9280ccd3585b
VikramanKesavan <tivikram@yahoo.co.in>
2022-08-25 10:01:25 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { of, from } from 'rxjs';
+import { of, from, map } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
 
   ngOnInit() {
+    // Creation of observable using of and from
     of(2, 4, 6, 8).subscribe((item) => console.log(item));
     from([2, 3, 4, 5, 6]).subscribe({
       next: (item) => console.log(`resulting item..${item}`),
@@ -23,5 +24,15 @@ export class AppComponent implements OnInit {
       error: (err) => console.log(`error occured ${err}`),
       complete: () => console.log(`complete`),
     });
+
+    // Operators
+    // source observable is the root of the data
+    of(2, 4, 3, 6)
+      .pipe(map((item) => item * 2))
+      .subscribe({
+        next: (item) => console.log(`map resulting item..${item}`),
+        error: (err) => console.log(`map error occured ${err}`),
+        complete: () => console.log(`map complete`),
+      });
   }
 }
