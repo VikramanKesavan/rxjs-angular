98e9301ce5001768c657639bc7a5b6efce386496
VikramanKesavan <tivikram@yahoo.co.in>
2022-08-25 10:11:26 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { of, from, map } from 'rxjs';
+import { of, from, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     // Creation of observable using of and from
+    console.log('---Obseravle/Observer creation----');
     of(2, 4, 6, 8).subscribe((item) => console.log(item));
     from([2, 3, 4, 5, 6]).subscribe({
       next: (item) => console.log(`resulting item..${item}`),
@@ -25,10 +26,38 @@ export class AppComponent implements OnInit {
       complete: () => console.log(`complete`),
     });
 
+    console.log('-----map Operator----');
     // Operators
     // source observable is the root of the data
+    // map operator
     of(2, 4, 3, 6)
       .pipe(map((item) => item * 2))
+      .subscribe({
+        next: (item) => console.log(`map resulting item..${item}`),
+        error: (err) => console.log(`map error occured ${err}`),
+        complete: () => console.log(`map complete`),
+      });
+
+    console.log('----tap Operator----');
+    // tap operator
+    of(2, 4, 3, 6)
+      .pipe(
+        map((item) => item * 2),
+        tap((item) => console.log(`$item am unchanged, but a here :)`))
+      )
+      .subscribe({
+        next: (item) => console.log(`map resulting item..${item}`),
+        error: (err) => console.log(`map error occured ${err}`),
+        complete: () => console.log(`map complete`),
+      });
+
+    console.log('----take Operator----');
+    of(2, 4, 3, 6)
+      .pipe(
+        map((item) => item * 2),
+        take(2),
+        tap((item) => console.log(`$item am unchanged, but a here :)`))
+      )
       .subscribe({
         next: (item) => console.log(`map resulting item..${item}`),
         error: (err) => console.log(`map error occured ${err}`),
